c5bb2d2c3048b4a483883c8a481f21cc37daf707
nudomkes <nythi.udomkesmalee@jpl.nasa.gov>
2018-10-09 10:37:15 -0700

    preliminary database model

---
+++ syllashare/user_data/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+# | ID | SCHOOL NAME | CITY | STATE |
+class School(models.Model):
+    name = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    state = models.CharField(max_length=16)
+
+
+# | ID | EVENT TYPE |
+class EventType(models.Model):
+    event_type = models.CharField(max_length=32)
+
+
+# | ID | SCHOOL | CLASS NAME | EVENT NAME | EVENT TYPE |
+class SyllabusEvent(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)  # if school is delete, do delete relevant entries
+    class_name = models.CharField(max_length=256)
+    entry_name = models.CharField(max_length=256)
+    event_type = models.ForeignKey(EventType, on_delete=models.CASCADE)  # if event type is deleted kill corr. entries
+    date = models.DateField()
+    # TODO: Implement Time. (Right now can only handle day, month, year)
+
+
+# | ID | FIRST NAME | LAST NAME | EMAIL | SCHOOL |
+class User(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+
+    # if school is somehow delete, don't delete user
+    school = models.ForeignKey(School, blank=True, on_delete=models.SET_DEFAULT)
+
+    # implicit Many-to-Many Table Created
+    syllabi = models.ManyToManyField(SyllabusEvent)
+
+
+# | ID | TOKEN | DEVICE TYPE | USER |
+class RefreshToken(models.Model):
+    token = models.CharField(max_length=128)
+    device_type = models.CharField(max_length=32)
+    expiration_time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
