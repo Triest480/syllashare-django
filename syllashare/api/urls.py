2a066631fed579fcb28263b8b06bbe32b6be719c
nudomkes <nythi.udomkesmalee@jpl.nasa.gov>
2018-11-08 13:56:44 -0800

    add searchuser endpoint and format bad words

--- syllashare/api/urls.py
+++ syllashare/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from syllatokens.views import exchange_google_code, reassign_google_token
-from user_data.views import get_class_schedule, modify_user, get_user, get_schools, search_classes
+from user_data.views import get_class_schedule, modify_user, get_user, get_schools, search_classes, search_users
 
 urlpatterns = [
     path(r'exchangegoogle', exchange_google_code),
     path(r'reassigngoogle', reassign_google_token),
     path(r'modifyuser', modify_user),
     path(r'getuser', get_user),
+    path(r'searchusers', search_users),
     path(r'getschools', get_schools),
     path(r'classschedule', get_class_schedule),
     path(r'searchclasses', search_classes),
