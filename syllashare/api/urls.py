9478348a9c1f5890f49c90be3f8b392798ea6c5b
nudomkes <nythi.udomkesmalee@jpl.nasa.gov>
2018-11-23 01:16:58 -0800

    enable api endpoint for class scan

--- syllashare/api/urls.py
+++ syllashare/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from syllatokens.views import exchange_google_code, reassign_google_token
 from user_data.views import get_class_schedule, modify_user, get_user, get_schools, search_classes, \
-    search_users, follow_class, unfollow_class
+    search_users, follow_class, unfollow_class, class_scan
 
 urlpatterns = [
     path(r'exchangegoogle', exchange_google_code),
@@ -15,4 +15,5 @@ urlpatterns = [
     path(r'searchclasses', search_classes),
     path(r'followclass', follow_class),
     path(r'unfollowclass', unfollow_class),
+    path(r'classscan', class_scan),
 ]
